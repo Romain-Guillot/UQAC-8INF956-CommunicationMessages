15e3671d4ef797c84c13aff966b27c03e518beb1
Romain Guillot <rguillot42@gmail.com>
2020-04-14 19:04:31 -0400

    Add sol / projects

---
+++ LogAnalysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommunicationMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
