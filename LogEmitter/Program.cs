d859a1640f700763df2f12e0cbb3de810478163c
Romain Guillot <rguillot42@gmail.com>
2020-04-14 20:31:05 -0400

    Opti

--- LogEmitter/Program.cs
+++ LogEmitter/Program.cs
@@ -7,6 +7,17 @@ namespace LogEmitter
     class Program
     {
         private static Random _random = new Random();
+        private static List<string> _randomSentence = new List<string>
+        {
+            "Would you like a drink of my smoothie?", "Hold on I need at least a few minutes!", "He landed a big trout.",
+            "I must get myself a new pair of glasses?", "It was a big one.", "What a big truck!",
+            "Tom has some pretty big shoes to fill.", "Why is it such a big deal?", "Why are your ears so big?",
+            "Very good job, I am so proud of you!", "We already know the truth.", "Do you know women who don’t want to dance?",
+            "What do you need it for?", "Shape up, or ship out!", "Do you understand?", "Would you like to have some coffee?",
+            "My legs feel like noodles!", "I’m not opening the door?", "Your biggest enemy is yourself.", "I can't wait to eat dinner!"
+        };
+        private static  Array _severities = Enum.GetValues(typeof(LogSeverity));
+        
         private static void Main(string[] args)
         {
             ILogger logger = new RabbitLogger();
@@ -22,39 +33,15 @@ namespace LogEmitter
                 Thread.Sleep(2000);
             }
         }
-            
-        private static LogSeverity RandomLogSeverity() {
-            var severities = Enum.GetValues(typeof(LogSeverity));
-            var randomSeverity = (LogSeverity) severities.GetValue(_random.Next(severities.Length));
-            return randomSeverity;
+
+        private static LogSeverity RandomLogSeverity()
+        {
+            return (LogSeverity) _severities.GetValue(_random.Next(_severities.Length));
         }
 
         private static string RandomMessage()
         {
-            var sentences = new List<string>
-            {
-                "Would you like a drink of my smoothie?",
-                "Hold on I need at least a few minutes!",
-                "He landed a big trout.",
-                "I must get myself a new pair of glasses?",
-                "It was a big one.",
-                "What a big truck!",
-                "Tom has some pretty big shoes to fill.",
-                "Why is it such a big deal?",
-                "Why are your ears so big?",
-                "Very good job, I am so proud of you!",
-                "We already know the truth.",
-                "Do you know women who don’t want to dance?",
-                "What do you need it for?",
-                "Shape up, or ship out!",
-                "Do you understand?",
-                "Would you like to have some coffee?",
-                "My legs feel like noodles!",
-                "I’m not opening the door?",
-                "Your biggest enemy is yourself.",
-                "I can't wait to eat dinner!"
-            };
-            return sentences[_random.Next(sentences.Count)];
+            return _randomSentence[_random.Next(_randomSentence.Count)];
         }
     }
 
